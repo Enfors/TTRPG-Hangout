5a8cef68daed4fb1ea69fb0a49f423f8527b0644
Christer Enfors <christer.enfors@gmail.com>
2025-07-11 13:15:40 +0200

    Improve file name handling

--- web.py
+++ web.py
@@ -11,8 +11,13 @@ def start():
 
 @app.route("/article/<string:article_file_name>")
 def article(article_file_name):
-    if not article_file_name[-5:] == ".html":
-        article_file_name += ".html"
-    with open(os.path.join(article_dir, article_file_name)) as html_file:
+
+    if article_file_name[-5:] == ".html":
+        article_file_name = article_file_name[:-5]
+
+    html_file_name = article_file_name + ".html"
+        
+    with open(os.path.join(article_dir, html_file_name)) as html_file:
         html = html_file.read()
-        return render_template("article.html", html=html)
+
+    return render_template("article.html", html=html)
