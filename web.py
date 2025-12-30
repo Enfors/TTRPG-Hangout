bb355ff1f55fde6693a479bdbb7aaa16314611cc
Christer Enfors <christer.enfors@gmail.com>
2025-12-30 16:24:43 +0100

    Add loading of TOC to start page

--- web.py
+++ web.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 import os
 
+TOC_FILE_NAME = "toc_ttrpg_hangout.html"
 
 app = Flask(__name__)
-article_dir = "../../RoamNotes"
+ARTICLE_DIR = "../../RoamNotes"
 
 @app.route("/")
 def start():
-    return render_template("start.html")
+    with open(os.path.join(ARTICLE_DIR, TOC_FILE_NAME)) as toc_file:
+        toc_html = toc_file.read()
+    
+    return render_template("start.html", toc_html=toc_html)
 
 @app.route("/article/<string:article_file_name>")
 def article(article_file_name):
@@ -17,7 +21,7 @@ def article(article_file_name):
 
     html_file_name = article_file_name + ".html"
         
-    with open(os.path.join(article_dir, html_file_name)) as html_file:
+    with open(os.path.join(ARTICLE_DIR, html_file_name)) as html_file:
         html = html_file.read()
 
     return render_template("article.html", html=html)
