b23b170cb39e5cd7f93d8d6823c5a2719192d862
Christer Enfors <christer.enfors@gmail.com>
2025-12-31 09:29:42 +0100

    Add proper handling of titles

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import os
 
+from bs4 import BeautifulSoup
+
 TOC_FILE_NAME = "toc_ttrpg_hangout.html"
 INTRO_FILE_NAME = "intro_ttrpg_hangout.html"
 
@@ -9,17 +11,12 @@ ARTICLE_DIR = "../../RoamNotes"
 
 @app.route("/")
 def start():
-    with open(os.path.join(ARTICLE_DIR, INTRO_FILE_NAME)) as intro_file:
-        intro_html = intro_file.read()
-    
-    with open(os.path.join(ARTICLE_DIR, TOC_FILE_NAME)) as toc_file:
-        toc_html = toc_file.read()
-
-    # os.chdir(ARTICLE_DIR)
+    intro_html = read_html(INTRO_FILE_NAME)
+    toc_html = read_html(TOC_FILE_NAME)
         
     return render_template("start.html", intro_html=intro_html, toc_html=toc_html)
 
-@app.route("/article/<string:article_file_name>")
+@app.route("/<string:article_file_name>")
 def article(article_file_name):
 
     if article_file_name[-5:] == ".html":
@@ -27,7 +24,23 @@ def article(article_file_name):
 
     html_file_name = article_file_name + ".html"
         
-    with open(os.path.join(ARTICLE_DIR, html_file_name)) as html_file:
-        html = html_file.read()
+    html, title = read_html_and_title(html_file_name)
 
-    return render_template("article.html", html=html)
+    return render_template("article.html", html=html, title=title)
+
+def read_html(file_name):
+    with open(os.path.join(ARTICLE_DIR, file_name)) as html_file:
+        return html_file.read()
+
+def read_html_and_title(file_name):
+    html = read_html(file_name)
+    title = None
+
+    soup = BeautifulSoup(html, 'html.parser')
+    h1_tag = soup.find('h1')
+
+    if h1_tag:
+        title = h1_tag.get_text()
+
+    return html, title
+
